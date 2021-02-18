01f8e97c9e9ff097292f3aef6b8f5f78aca128ab
coolcard <gaoz@sina.com>
2021-02-18 21:21:46 +0800

    deploy

---
+++ precache-manifest.c3fc99d1b20a7cbba81330eb1cdfa4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929658b1acf8c0173fa4",
+    "url": "/money-website/css/app.8ae3a906.css"
+  },
+  {
+    "revision": "152661f8c2e43b4856c6cc0371c2f5cb",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "929658b1acf8c0173fa4",
+    "url": "/money-website/js/app.99ea15f8.js"
+  },
+  {
+    "revision": "5d9045a879c3e74c4dbb",
+    "url": "/money-website/js/chunk-vendors.086bd3ca.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
